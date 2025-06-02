d2f16f71170bb36ae8561ccc7f7a2c4a2c9d106a
ISOREX <isorex@icloud.com>
2025-06-02 20:37:11 +0200

    version increments for future pytauri v0.6.0 update

--- templates/{{ '.' }}/{{ project_name }}/src-tauri/src-python/{{ package_name }}/__init__.py
+++ templates/{{ '.' }}/{{ project_name }}/src-tauri/src-python/{{ package_name }}/__init__.py
@@ -29,10 +29,8 @@ async def greet(body: Person) -> Greeting:
 def main() -> int:
     with start_blocking_portal("asyncio") as portal:  # or `trio`
         app = builder_factory().build(
-            BuilderArgs(
-                context=context_factory(),
-                invoke_handler=commands.generate_handler(portal),
-            )
+            context=context_factory(),
+            invoke_handler=commands.generate_handler(portal),
         )
         exit_code = app.run_return()
         return exit_code
